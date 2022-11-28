0a883050d90c0c7b13fa0426ec5541ec581196a5
Vitaly <vitaly.rybkin@gmail.com>
2022-11-28 13:10:45 +0300

    fixed: logic changed

--- TASK01_10/Program.cs
+++ TASK01_10/Program.cs
@@ -30,33 +30,22 @@ string CheckInput (string get_string) {
 
 Console.Clear();
 
-//MATH VERSION *** WARNING *** WORKS ONLY WITH #-DIGIT NUMS!!!
+//MATH VERSION
 Console.WriteLine("MATH VERSION...\n");
-Console.WriteLine("*** ACHTUNG *** WORKS PROPERLY ONLY WITH 3-DIGIT NUMS!\n");
 Console.Write("Enter a number: ");
 string text = Console.ReadLine() ?? "";
 string get_string = CheckInput (text);
 int num = Convert.ToInt32(get_string);
 
-num = num / 10 % 10;
+while (num >= 100) num /= 10;
+    int res = num % 10;
 
-Console.Write($"Second digit of you num is {num}");
+Console.Write($"Second digit of you num is {res}");
 
+//STRING VERSION
 Console.WriteLine("\n\nSTRING VERSION...\n");
 Console.Write("Enter a number: ");
 text = Console.ReadLine() ?? "";
 get_string = CheckInput (text);
-
-
-char[] get_num = get_string.ToCharArray(0, get_string.Length);
-
-// OUTPUT OPTION LIST
-// Output option 1.
-// int res = get_num[0] - '0';
-// Outout option 2.
-// int res = Convert.ToInt32(new string(get_num[1], 1));
-// Outout option 3.
-int res = Convert.ToInt32(get_num[1].ToString());
-// ... or write an expression straight into output (instead of res) 
-
-Console.WriteLine($"\nSecond digit of you num is {res}");
+res = Convert.ToInt32(get_string[1].ToString());
+Console.WriteLine($"Second digit of you num is {Convert.ToInt32(get_string[1].ToString())}");
